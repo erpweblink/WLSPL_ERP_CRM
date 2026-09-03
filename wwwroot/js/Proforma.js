5d34c1bc42cfb26364d30b8ac4c1dada6fe95c7e
ShubhamP <Admin@DESKTOP-TPRFDD8>
2026-09-03 11:19:44 +0530

    Update new changes on the proforma

--- wwwroot/js/Proforma.js
+++ wwwroot/js/Proforma.js
@@ -759,15 +759,8 @@
 
                     $("#ddlReverseCharge").val(hdr.reverseCharge || "N").trigger("change");
                     $("#ddlAgainstBy").val(hdr.againstBy || "Direct").trigger("change");
-                    BindStateList(function () {
-                        $("#ddlBillState").val(hdr.billState || "").trigger("change");
-                    });
-
-                    // If AgainstBy is "Direct", BindAgainstNumber never runs, so set + lock directly here.
-                    if ((hdr.againstBy || "Quotation") === "Quotation") {
-                        $("#ddlAgainstNo").val(hdr.againstNo || "");
-                    }
-                    $("#ddlAgainstNo").prop("disabled", true);
+                    $("#ddlBillState").val(hdr.billState || "").trigger("change");
+                    $("#ddlAgainstNo").val(hdr.againstNo || "").trigger("change");
 
                     $("#txtProformaDate").val(formatDateToDDMMYYYY(hdr.proformaDate));
 
