b541c13e08e9198327082e54a186c4df803ea3b0
Nikhil More <nm@DESKTOP-V384QLO>
2026-09-10 11:44:38 +0530

    Shortcuts new function changes by Nikhil

--- Controllers/ShortcutController.cs
+++ Controllers/ShortcutController.cs
@@ -1,19 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using WEBLINK_CRM.Models;
+using WLSPL_ERP_CRM.repository;
 
 public class ShortcutController : Controller
 {
-    private readonly List<ShortcutItem> _shortcuts;
+    private readonly IShortcutRepo _shortcuts;
 
-    public ShortcutController(IOptions<List<ShortcutItem>> shortcuts)
+    public ShortcutController(IShortcutRepo shortcut)
     {
-        _shortcuts = shortcuts.Value;
+        _shortcuts = shortcut;
     }
 
     [HttpGet]
     public IActionResult GetShortcuts()
     {
-        return Json(_shortcuts);
+        var shortcuts = _shortcuts.GetShortcutsAsync().Result;
+        return Json(shortcuts);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchEmployees(string q)
+    {
+        string userRole = HttpContext.Session.GetString("Role")?.ToString() ?? "NA";
+        string userCode = HttpContext.Session.GetString("EmpCode")?.ToString() ?? "NA";
+
+        if (string.IsNullOrWhiteSpace(q))
+            return Json(new List<object>());
+
+        var employees = await _shortcuts.SearchEmployees(q,userRole,userCode);
+        return Json(employees);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchCompanies(string q)
+    {
+        string userRole = HttpContext.Session.GetString("Role") ?? "NA";
+        string userCode = HttpContext.Session.GetString("EmpCode") ?? "NA";
+
+        if (string.IsNullOrWhiteSpace(q))
+            return Json(new List<object>());
+
+        var companies = await _shortcuts.SearchCompanies(q, userRole, userCode);
+        return Json(companies);
     }
 }
